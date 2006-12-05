6cb2011b2fcd0cea40d789e9efa22dc48191d63f
Martin Jolicoeur <snmpb1@gmail.com>
2006-12-05 02:49:56 +0000

    marker widget cleanup
    git-svn-id: https://svn.code.sf.net/p/snmpb/svn/trunk@134 d4971f4b-c5c8-4039-af2b-e5317b2b9980

--- app/markerwidget.cpp
+++ app/markerwidget.cpp
@@ -1,20 +1,12 @@
 #include <QtGui>
-#include <qtextedit.h>
 #include <qpainter.h>
-#include <qwidget.h>
 #include "markerwidget.h"
 
-MarkerWidget::MarkerWidget(QWidget* parent )
-    : QWidget( parent )
-      ,m_clickChangesBPs(true)
-      ,m_changeBookmarksAllowed(false)
-      ,m_changeBreakpointsAllowed(false)
-      ,m_bookmarkDescr("Bookmark")
-      ,m_breakpointDescr("Breakpoint")
+MarkerWidget::MarkerWidget(QWidget* parent ):QWidget( parent )
 {
     setAttribute(Qt::WA_StaticContents);
 
-    m_pixmap = QPixmap( ":/images/scalar.png" );
+    m_pixmap = QPixmap( ":/images/marker.png" );
 
     setFixedWidth( 15 );
 }
@@ -27,7 +19,7 @@ void MarkerWidget::setTextEditor(QTextEdit* editor)
 {
     m_editor = editor;
 
-    connect( (QObject*)m_editor->verticalScrollBar(), SIGNAL( valueChanged( int ) ),
+    connect( m_editor->verticalScrollBar(), SIGNAL( valueChanged( int ) ),
              this, SLOT( doRepaint() ) );
     connect( m_editor, SIGNAL( textChanged() ),
              this, SLOT( doRepaint() ) );
@@ -37,36 +29,42 @@ void MarkerWidget::setTextEditor(QTextEdit* editor)
 
 void MarkerWidget::paintEvent( QPaintEvent* /*e*/ )
 {
-    QPainter mypainter( this );
+    QPainter painter( this );
 
     QTextBlock currentBlock = m_editor->document()->begin();
-    int l = 1;
     int viewPortHeight = m_editor->maximumViewportSize().height();
     int yOffset = m_editor->verticalScrollBar()->value();
 
+    // Loop through the blocks
     while(currentBlock.isValid())
     {
-        int yCoord = (int)currentBlock.layout()->position().y();
-        int lineHeight = (int)currentBlock.layout()->boundingRect().height();
-
-//        printf("yOffset: %d, yCoord: %d, %f\n", yOffset, yCoord, currentBlock.layout()->boundingRect().height());
+        // Found a line that needs a marker
         if(currentBlock.userState() == 0xFF)
-            if ((yCoord + lineHeight >= yOffset) && (yCoord - yOffset <= viewPortHeight))
-                mypainter.drawPixmap(0, yCoord - yOffset + (((lineHeight-m_pixmap.height())/2)), m_pixmap );
+        {
+            int yCoord = (int)currentBlock.layout()->position().y();
+            int lineHeight = (int)currentBlock.layout()->boundingRect().height();
+            int imageOffset = (lineHeight-m_pixmap.height())/2;
+
+            // Draw the marker pixmap if visible
+            if ((yCoord + lineHeight >= yOffset) && 
+                (yCoord - yOffset <= viewPortHeight))
+                painter.drawPixmap(0, yCoord - yOffset + imageOffset, m_pixmap);
+        }
 
         currentBlock = currentBlock.next();
-        l++;
     };
 
-    mypainter.end();
+    painter.end();
 }
 
 void MarkerWidget::setMarker( int line )
 {
     QTextBlock currentBlock = m_editor->document()->begin();
+    QTextBlock foundBlock;
     int l = 1;
-    int changed = 0;
+    int changed = 0, found = 0;
 
+    // Loop through the blocks
     while(currentBlock.isValid())
     {
         if (l == line)
@@ -76,41 +74,45 @@ void MarkerWidget::setMarker( int line )
                 changed = 1;
                 currentBlock.setUserState(0xFF);
             }
-            break;
+
+            found = 1;
+            foundBlock = currentBlock;
         }
+        else
+        {
+            // Clear all other lines
+            if (currentBlock.userState() == 0xFF)
+            {
+                changed = 1;
+                currentBlock.setUserState(0x00);
+            }
+        }
+
         currentBlock = currentBlock.next();
         l++;
     };
 
-//    QTextCursor tc = m_editor->cursorForPosition ( QPoint(0, (int)currentBlock.layout()->position().y()) );
-    m_editor->verticalScrollBar()->setValue((int)currentBlock.layout()->position().y());
-//    m_editor->setTextCursor(tc);
-//    m_editor->ensureCursorVisible();
+    if (found)
+    {
+        // Change scrollbar to put the marker visible in the middle of the editor
+        int halfViewPortHeight = m_editor->maximumViewportSize().height()/2;
+        int yCoord = (int)foundBlock.layout()->position().y();
+        int yAdjust = (yCoord < halfViewPortHeight)?yCoord : halfViewPortHeight;
+        int halfLineHeight = (int)foundBlock.layout()->boundingRect().height()/2;
+
+        m_editor->verticalScrollBar()->setValue(yCoord - yAdjust);
+
+        // Set the cursor position to the marker line
+        QPoint cursorPos(0, yAdjust+halfLineHeight);
+        QTextCursor tc = m_editor->cursorForPosition(cursorPos);
+
+        m_editor->setTextCursor(tc);
+
+        // Finally, set the focus to the editor
+        m_editor->setFocus(Qt::OtherFocusReason);
+    }
 
     if (changed)
         doRepaint();
-}
-
-void MarkerWidget::setPixmap(int mt, const QPixmap & pm)
-{
-  if (mt)
-    m_pixmap = pm;
-}
-
-void MarkerWidget::setDescription(int mt, const QString & s)
-{
-  switch (mt) {
-  case 1: m_bookmarkDescr = s; break;
-  case 2: m_breakpointDescr = s; break;
-  default: break;
-  }
-}
-
-void MarkerWidget::setMarksUserChangable(int markMask)
-{
-  m_changeBookmarksAllowed   = (markMask & 1) ? true : false;
-  m_changeBreakpointsAllowed = (markMask & 2) ? true : false;
-
-  doRepaint();
 }
 
--- app/markerwidget.h
+++ app/markerwidget.h
@@ -1,49 +1,29 @@
-#ifndef __markerwidget_h
-#define __markerwidget_h
+#ifndef MARKERWIDGET_H
+#define MARKERWIDGET_H
 
 #include <qwidget.h>
 #include <qpixmap.h>
-#include <qmap.h>
 #include <qtextedit.h>
-
-struct Mark
-{
-    int line;
-    int type;
-};
 
 class MarkerWidget: public QWidget
 {
     Q_OBJECT
 public:
-    MarkerWidget( QWidget* = 0);
+    MarkerWidget(QWidget* = 0);
     virtual ~MarkerWidget();
 
     void setTextEditor(QTextEdit*);
     void setMarker(int line);
 
-    virtual void setPixmap(int, const QPixmap &);
-    virtual void setDescription(int, const QString &);
-    virtual void setMarksUserChangable(int markMask);
-
 public slots:
-    void doRepaint() { repaint( ); }
+    void doRepaint() { repaint(); }
 
 protected:
     virtual void paintEvent( QPaintEvent* );
 
-signals:
-    void markChanged( struct Mark mark, int action_type );
-    void marksChanged();
-
 private:
-    QTextEdit*              m_editor;
-    QPixmap                 m_pixmap;
-    bool                    m_clickChangesBPs;
-    bool                    m_changeBookmarksAllowed;
-    bool                    m_changeBreakpointsAllowed;
-    QString                 m_bookmarkDescr;
-    QString                 m_breakpointDescr;
+    QTextEdit  *m_editor;
+    QPixmap    m_pixmap;
 };
 
-#endif // __markerwidget_h
+#endif /* MARKERWIDGET_H */
