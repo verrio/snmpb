cecad715c09d0c6c0ce75a5bfef80a3f86f8beb5
Martin Jolicoeur <snmpb1@gmail.com>
2008-04-13 07:42:33 +0000

    Added banner
    git-svn-id: https://svn.code.sf.net/p/snmpb/svn/trunk@229 d4971f4b-c5c8-4039-af2b-e5317b2b9980

--- app/mibhighlighter.cpp
+++ app/mibhighlighter.cpp
@@ -1,3 +1,22 @@
+/*
+    Copyright (C) 2004-2008  Martin Jolicoeur (snmpb1@gmail.com) 
+
+    This file is part of the SnmpB project 
+    (http://sourceforge.net/projects/snmpb)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 #include <QtGui>
 #include "mibhighlighter.h"
 
