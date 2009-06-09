8eba1c62dc5245a2189637a7335e115d147c8804
Martin Jolicoeur <snmpb1@gmail.com>
2009-06-09 06:00:30 +0000

    Added Mib Selection class for SET operation and for Multiple Varbinds GUI; SET now tested properly
    git-svn-id: https://svn.code.sf.net/p/snmpb/svn/trunk@351 d4971f4b-c5c8-4039-af2b-e5317b2b9980

---
+++ app/mibselection.cpp
@@ -0,0 +1,483 @@
+/*
+    Copyright (C) 2004-2008  Martin Jolicoeur (snmpb1@gmail.com) 
+
+    This file is part of the SnmpB project 
+    (http://sourceforge.net/projects/snmpb)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#include <qmessagebox.h>
+#include <QDate>
+#include <QDialog>
+#include <QValidator>
+
+#include "mibselection.h"
+#include "agent.h"
+
+// If isget is false, it is a set operation
+MibSelection::MibSelection(Snmpb *snmpb, bool isget)
+{
+    s = snmpb;
+
+    validator = NULL;
+    validator64 = NULL;
+    val_cb = NULL;
+    val_le = NULL;
+
+    type = NULL;
+
+    doget = isget;
+
+    dprompt = new QDialog(NULL, Qt::WindowTitleHint);
+    dprompt->resize(400, 500);
+
+    gl = new QGridLayout(dprompt);
+    oidlabel = new QLabel("<b>OID:</b>", dprompt);
+    gl->addWidget(oidlabel, 1, 0, 1, 1);
+
+    oid_le = new QLineEdit("", dprompt);
+    connect(oid_le, SIGNAL(editingFinished(void)), 
+            this, SLOT(GetOidLe(void)));
+    gl->addWidget(oid_le, 2, 0, 1, 1);
+
+    bmv = new BasicMibView(dprompt);
+    bmv->RegisterToLoader(s->MibLoaderObj());
+    bmv->Populate();
+    gl->addWidget(bmv, 3, 0, 1, 1);
+
+    syntaxlabel = new QLabel("<b>Syntax:</b>", dprompt);
+    gl->addWidget(syntaxlabel, 4, 0, 1, 1);
+
+    syntax_cb = new QComboBox(dprompt);
+    gl->addWidget(syntax_cb, 5, 0, 1, 1);
+    syntax_cb->addItem("Integer32", QVariant(sNMP_SYNTAX_INT32));
+    syntax_cb->addItem("UInteger32", QVariant(sNMP_SYNTAX_UINT32));
+    syntax_cb->addItem("Counter32", QVariant(sNMP_SYNTAX_CNTR32));
+    syntax_cb->addItem("Gauge32", QVariant(sNMP_SYNTAX_GAUGE32));
+    syntax_cb->addItem("Octet String", QVariant(sNMP_SYNTAX_OCTETS));
+    syntax_cb->addItem("Bits", QVariant(sNMP_SYNTAX_BITS));
+    syntax_cb->addItem("Object Identifier", QVariant(sNMP_SYNTAX_OID));
+    syntax_cb->addItem("Counter64", QVariant(sNMP_SYNTAX_CNTR64));
+    syntax_cb->addItem("Timeticks", QVariant(sNMP_SYNTAX_TIMETICKS));
+    syntax_cb->addItem("IP address", QVariant(sNMP_SYNTAX_IPADDR));
+    syntax_cb->addItem("Opaque", QVariant(sNMP_SYNTAX_OPAQUE));
+
+    connect(syntax_cb, SIGNAL(currentIndexChanged(int)), 
+            this, SLOT(GetSyntaxCb(int)));
+
+    vallabel = new QLabel("<b>Value:</b>", dprompt);
+    gl->addWidget(vallabel, 6, 0, 1, 1);
+
+    val_le = new QLineEdit(dprompt);
+    gl->addWidget(val_le, 7, 0, 1, 1);
+
+    val_cb = new QComboBox(dprompt);
+    val_cb->setVisible(false);
+    gl->addWidget(val_cb, 8, 0, 1, 1);
+
+    infolabel = new QLabel("");
+    gl->addWidget(infolabel, 9, 0, 1, 1);
+
+    dprompt->setWindowTitle(QApplication::translate("Dialog", 
+                            doget?"Get":"Set", 0, QApplication::UnicodeUTF8));
+    box = new QDialogButtonBox(QDialogButtonBox::Ok|QDialogButtonBox::Cancel, 
+                               Qt::Horizontal, dprompt);
+    gl->addWidget(box, 10, 0, 1, 1);
+    connect(box, SIGNAL(accepted()), this, SLOT(OKButtonPressed()));
+    connect(box, SIGNAL(rejected()), dprompt, SLOT(reject()));
+    QMetaObject::connectSlotsByName(dprompt);
+}
+
+MibSelection::~MibSelection()
+{
+    if (gl)
+        delete gl;
+    if (oidlabel)
+        delete oidlabel;
+    if (syntaxlabel)
+        delete syntaxlabel;
+    if (vallabel)
+        delete vallabel;
+    if (infolabel)
+        delete infolabel;
+    if (validator)
+        delete validator;
+    if (validator64)
+        delete validator64;
+    if (box)
+        delete box;
+    if (val_cb)
+        delete val_cb;
+    if (syntax_cb)
+        delete syntax_cb;
+    if (val_le)
+        delete val_le;
+    if (oid_le)
+        delete oid_le;
+    if (bmv)
+        delete bmv;
+    if (dprompt)
+        delete dprompt;
+}
+
+Vb *MibSelection::GetVarbind(void)
+{
+    return &vb;
+}
+
+// Callback when the value combobox value changes 
+void MibSelection::GetValueCb(int index)
+{
+    result_string = val_cb->itemData(index).toString();
+    val_le->setText(result_string);
+}
+
+// Callback when the value linedit edition is finished
+void MibSelection::GetValueLe(void)
+{
+    result_string = val_le->text();
+}
+
+// Callback when the OID linedit edition is finished
+void MibSelection::GetOidLe(void)
+{
+    result_oid = oid_le->text();
+}
+
+// Callback when the syntax combobox value changes
+void MibSelection::GetSyntaxCb(int index)
+{
+    result_syntax = syntax_cb->itemData(index).toInt();
+    val_le->clear();
+    SetValueWidget();
+}
+
+// Callback when an item is selected in the MIB tree window 
+void MibSelection::GetSelectedOid(const QString& oid)
+{
+    SetOidInfoType(oid);
+    SetSyntax();
+    SetValueWidget();
+}
+
+// Callback when the user presses the OK button 
+void MibSelection::OKButtonPressed(void)
+{
+    // Fill-in pdu and do validation checking
+    switch (result_syntax)
+    {
+        case sNMP_SYNTAX_INT32:
+            {
+                SnmpInt32 v(result_string.toInt());
+                vb.set_value(v);
+                break;
+            }
+        case sNMP_SYNTAX_CNTR32:
+            {
+                Counter32 v((unsigned long)result_string.toUInt());
+                vb.set_value(v);
+                break;
+            }
+        /*case sNMP_SYNTAX_UINT32:*/
+        case sNMP_SYNTAX_GAUGE32:
+            {
+                Gauge32 v((unsigned long)result_string.toUInt());
+                vb.set_value(v);
+                break;
+            }
+        case sNMP_SYNTAX_OCTETS:
+        case sNMP_SYNTAX_BITS: /* Obsoleted ? */
+            {
+                OctetStr octetstr(result_string.toLatin1().data());
+                if (octetstr.valid())
+                    vb.set_value(octetstr);
+                else
+                {
+                    QMessageBox::critical( NULL, "MIB Operation", 
+                            "Invalid Octet String value",
+                            QMessageBox::Ok, Qt::NoButton);
+                    return;
+                } 
+                break;
+            }
+        case sNMP_SYNTAX_OPAQUE:
+            {
+                OpaqueStr opaquestr(result_string.toLatin1().data());
+                if (opaquestr.valid())
+                    vb.set_value(opaquestr);
+                else
+                {
+                    QMessageBox::critical( NULL, "MIB Operation", 
+                            "Invalid Opaque String value",
+                            QMessageBox::Ok, Qt::NoButton);
+                    return;
+                } 
+                break;
+            }
+        case sNMP_SYNTAX_OID:
+            {
+                Oid oid(result_string.toLatin1().data());
+                if (oid.valid())
+                    vb.set_value(oid);
+                else
+                {
+                    QMessageBox::critical( NULL, "MIB Operation", 
+                            "Invalid Object Identifier value",
+                            QMessageBox::Ok, Qt::NoButton);
+                    return;
+                }
+                break;
+            }
+        case sNMP_SYNTAX_CNTR64:
+            vb.set_value(Counter64::ll_to_c64(result_string.toULongLong()));
+            break;
+        case sNMP_SYNTAX_TIMETICKS:
+            {
+                TimeTicks timeticks(result_string.toUInt());
+                if (timeticks.valid())
+                    vb.set_value(timeticks);
+                else
+                {
+                    QMessageBox::critical( NULL, "MIB Operation", 
+                            "Invalid TimeTick value",
+                            QMessageBox::Ok, Qt::NoButton);
+                    return;
+                }
+                break;
+            }
+        case sNMP_SYNTAX_IPADDR:
+            {
+                IpAddress ipaddress(result_string.toLatin1().data());
+                if (ipaddress.valid())
+                    vb.set_value( ipaddress);
+                else
+                {
+                    QMessageBox::critical( NULL, "MIB Operation", 
+                            "Invalid IP address value",
+                            QMessageBox::Ok, Qt::NoButton);
+                    return;
+                } 
+                break;
+            }
+        default:
+            break;
+    }
+
+    dprompt->accept();
+}
+
+void MibSelection::SetOidInfoType(const QString& oid)
+{
+    Oid poid(oid.toLatin1().data());
+
+    QString outoid = oid;
+    QString outinfo = "";
+    type = NULL;
+
+    if (poid.valid() == true)
+    {
+        SmiNode *node = smiGetNodeByOID(poid.len(), (SmiSubid*)&(poid[0]));
+        if (node && (node->oidlen >= poid.len()))
+        {
+            SmiType *thetype = smiGetNodeType(node);
+            SmiRange *r;
+
+            // If a column object, ask the user to select the instance ...
+            if (node->nodekind == SMI_NODEKIND_COLUMN)
+            {
+                QString inst;
+                // Pop-up the selection dialog
+                if (s->AgentObj()->SelectTableInstance(oid, inst))
+                    outoid = oid + "." + inst;
+                else
+                    outoid = oid; // Invalid, but no instance selected ...
+            }
+            else
+                outoid = oid + ".0";
+
+            if (thetype)
+            {
+                if (smiGetFirstRange(thetype))
+                {
+                    outinfo += QString("<br><b>Range:</b> ");
+                    for (r = smiGetFirstRange(thetype); r; r = smiGetNextRange(r))
+                    {
+                        outinfo += QString("%1 .. %2")
+                                           .arg(r->minValue.value.unsigned64)
+                                           .arg(r->maxValue.value.unsigned64);
+                        if (smiGetNextRange(r))
+                            outinfo += ", ";
+                    }
+                }
+            }
+
+            if (node->access != SMI_ACCESS_READ_WRITE)
+                outinfo += "<br><b><font color=red>WARNING: object is not writable!</font></b>";
+
+            type = thetype;
+        }
+    }
+
+    oid_le->setText(outoid);
+    result_oid = oid_le->text();
+
+    infolabel->setText(outinfo);
+}
+
+void MibSelection::SetValueWidget(void)
+{
+    SmiNamedNumber  *nn;
+
+    val_cb->clear();
+    val_cb->setVisible(false);
+
+    if (type && smiGetFirstNamedNumber(type) && 
+        (type->basetype != SMI_BASETYPE_BITS))
+    {
+        val_cb->setVisible(true);
+
+        for (nn = smiGetFirstNamedNumber(type); nn; nn = smiGetNextNamedNumber(nn))
+            val_cb->addItem(QString("%1 (%2)")
+                            .arg(nn->name).arg(nn->value.value.unsigned32), 
+                            QVariant((unsigned int)nn->value.value.unsigned32));
+        connect(val_cb, SIGNAL(currentIndexChanged(int)), 
+                this, SLOT(GetValueCb(int)));
+    }
+
+    switch (result_syntax)
+    {
+        case sNMP_SYNTAX_OCTETS:
+        case sNMP_SYNTAX_BITS:
+        case sNMP_SYNTAX_OPAQUE:
+        case sNMP_SYNTAX_OID:
+        case sNMP_SYNTAX_IPADDR:
+            val_le->setMaxLength(65535);
+            val_le->setValidator(NULL);
+            break;
+        case sNMP_SYNTAX_INT32:
+            if (validator)
+                delete validator;
+            validator = new IntValidator(false, val_le);
+            val_le->setValidator(validator);
+            break;
+        case sNMP_SYNTAX_CNTR64:
+            if (validator64)
+                delete validator64;
+            validator64 = new UInt64Validator(val_le);
+            val_le->setValidator(validator64);
+            break;
+        default:
+            if (validator)
+                delete validator;
+            validator = new IntValidator(true, val_le);
+            val_le->setValidator(validator);
+    }
+
+    connect(val_le, SIGNAL(editingFinished(void)), 
+            this, SLOT(GetValueLe(void)));
+}
+
+void MibSelection::SetSyntax(void)
+{
+    if (type)
+    {
+        switch (type->basetype)
+        {
+            case SMI_BASETYPE_INTEGER32:
+            case SMI_BASETYPE_ENUM:
+                syntax_cb->setCurrentIndex(0);
+                break;
+            case SMI_BASETYPE_UNSIGNED32:
+                if (type->name)
+                {
+                    if (!strcmp(type->name, "TimeTicks"))
+                    {
+                        syntax_cb->setCurrentIndex(8);
+                        break;
+                    }
+                    else if (!strcmp(type->name, "Counter32") || 
+                            !strcmp(type->name, "COUNTER"))
+                    {
+                        syntax_cb->setCurrentIndex(2);
+                        break;
+                    }
+                    else if (!strcmp(type->name, "Gauge32") || 
+                            !strcmp(type->name, "GAUGE"))
+                    {
+                        syntax_cb->setCurrentIndex(3);
+                        break;
+                    }
+                }
+                syntax_cb->setCurrentIndex(1);
+                break;
+            case SMI_BASETYPE_OCTETSTRING:
+                if (type->name)
+                {
+                    if (!strcmp(type->name, "IpAddress"))
+                    {
+                        syntax_cb->setCurrentIndex(9);
+                        break;
+                    }
+                    else if (!strcmp(type->name, "Opaque"))
+                    {
+                        syntax_cb->setCurrentIndex(10);
+                        break;
+                    }
+                }
+                syntax_cb->setCurrentIndex(4);
+                break;
+            case SMI_BASETYPE_BITS: 
+                syntax_cb->setCurrentIndex(5);
+                break;
+            case SMI_BASETYPE_OBJECTIDENTIFIER:
+                syntax_cb->setCurrentIndex(6);
+                break;
+            case SMI_BASETYPE_UNSIGNED64:
+                syntax_cb->setCurrentIndex(7);
+                break;
+            default:
+                break;
+        }
+    }
+    else // default
+        syntax_cb->setCurrentIndex(4);
+}
+
+bool MibSelection::run(const QString& oid)
+{
+    bool status = false;
+
+    bmv->SelectFromOid(oid);
+
+    connect( bmv, SIGNAL( SelectedOid(const QString&) ), 
+             this, SLOT( GetSelectedOid(const QString&) ));
+
+    SetOidInfoType(oid); 
+    SetSyntax();
+    SetValueWidget();
+
+    val_le->setFocus(Qt::OtherFocusReason);
+
+    // Show the gui to the user. If OK is pressed, construct and send the packet
+    if (dprompt->exec())
+    {
+        vb.set_oid(Oid(result_oid.toLatin1().data()));
+        status = true;
+    }
+
+    disconnect( bmv, SIGNAL( SelectedOid(const QString&) ), 0, 0);
+
+    return status;
+}
+
---
+++ app/mibselection.h
@@ -0,0 +1,166 @@
+/*
+    Copyright (C) 2004-2008  Martin Jolicoeur (snmpb1@gmail.com) 
+
+    This file is part of the SnmpB project 
+    (http://sourceforge.net/projects/snmpb)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#ifndef MIBSELECTION_H
+#define MIBSELECTION_H
+
+#include <QValidator>
+#include <QDialog>
+#include <QDialogButtonBox>
+
+#include "snmpb.h"
+#include "snmp_pp/snmp_pp.h"
+
+// Class used to validate set operations on Counter32/Unsigned32/Gauge32/Integer...
+class IntValidator: public QValidator
+{
+public:
+    IntValidator(bool is_unsigned, QObject *parent = 0): QValidator(parent)
+    {
+        is_uint = is_unsigned;
+    }
+
+    // -2147483648 .. 4294967295
+    State validate (QString& input, int& pos) const
+    {
+        QRegExp r("\\-?\\d{0,10}");
+        if (r.exactMatch(input))
+        {
+            bool ok;
+            if (is_uint == true)
+            {
+                unsigned int uval = input.toUInt(&ok);
+                if ( ((ok == true) && (uval <= 4294967295U)) || 
+                     ((ok == false) && (!input.size())) )
+                    return Acceptable;
+                else
+                    return Invalid;
+            }
+            else
+            {
+                int val = input.toInt(&ok);
+                if ( ((ok == true) && (val >= (-2147483647-1)) && 
+                                      (val <= 2147483647)) || 
+                     ((ok == false) && (!input.size() || (input.size() == 1))) )
+                    return Acceptable;
+                else
+                    return Invalid;
+            }
+        }
+        else
+        {
+            if (const_cast<QRegExp &>(r).matchedLength() == input.size())
+                return Intermediate;
+            else
+            {
+                pos = input.size();
+                return Invalid;
+            }
+        }
+    }
+
+private:
+    bool is_uint;
+};
+
+// Class used to validate set operations on Counter64
+class UInt64Validator: public QValidator
+{
+public:
+    UInt64Validator(QObject *parent = 0): QValidator(parent) {}
+
+    State validate (QString& input, int& pos) const
+    {
+        QRegExp r("\\-?\\d{0,20}");
+        if (r.exactMatch(input))
+        {
+            bool ok;
+            unsigned long long uval = input.toULongLong(&ok);
+            if ( ((ok == true) && (uval <= 0xFFFFFFFFFFFFFFFFULL)) || 
+                 ((ok == false) && (!input.size())) )
+                return Acceptable;
+            else
+                return Invalid;
+        }
+        else
+        {
+            if (const_cast<QRegExp &>(r).matchedLength() == input.size())
+                return Intermediate;
+            else
+            {
+                pos = input.size();
+                return Invalid;
+            }
+        }
+    }
+};
+
+class MibSelection: public QObject
+{
+    Q_OBJECT
+    
+public:
+    MibSelection(Snmpb *snmpb, bool isget);
+    ~MibSelection();
+
+    bool run(const QString& oid);
+    Vb *GetVarbind(void);
+
+public slots:
+    void GetValueCb(int index);
+    void GetValueLe(void);
+    void GetOidLe(void);
+    void GetSelectedOid(const QString& oid);
+    void GetSyntaxCb(int index);
+    void OKButtonPressed(void);
+
+protected:
+    void SetOidInfoType(const QString& oid);
+    void SetValueWidget(void);
+    void SetSyntax(void);
+
+private:
+    Snmpb *s;
+    Vb vb;
+    bool doget;
+  
+    QGridLayout *gl;
+    QLabel *oidlabel;
+    QLabel *syntaxlabel;
+    QLabel *vallabel;
+    QLabel *infolabel;
+    QDialogButtonBox *box;
+    QDialog *dprompt; 
+    QComboBox *val_cb;
+    QComboBox *syntax_cb;
+    QLineEdit *val_le;
+    QLineEdit *oid_le;
+    BasicMibView *bmv;
+ 
+    QString result_string;
+    int     result_int;
+    QString result_oid;
+    int     result_syntax;
+    SmiType *type;
+
+    IntValidator *validator;
+    UInt64Validator *validator64;
+};
+
+#endif /* MIBSELECTION_H */
